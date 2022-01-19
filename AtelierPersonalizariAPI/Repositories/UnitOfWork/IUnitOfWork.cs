d317931a9786f7a326841ce02024121171b50723
IOAN DAVID BLAJ <ioan.blaj@s.unibuc.ro>
2022-01-20 01:18:26 +0200

    -Removed WeatherForecast +Added repository pattern and unit of work

---
+++ AtelierPersonalizariAPI/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtelierPersonalizariAPI.Models;
+using AtelierPersonalizariAPI.Repositories.GenericRepository;
+
+namespace AtelierPersonalizariAPI.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<ProductTypeProvider> ProductTypeProviderRepository { get; }
+        GenericRepository<ProductType> ProductTypeRepository { get; }
+        GenericRepository<Product> ProductRepository { get; }
+        void Save();
+        void Dispose();
+    }
+}
---
+++ AtelierPersonalizariAPI/Repositories/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtelierPersonalizariAPI.Data;
+using AtelierPersonalizariAPI.Models;
+using AtelierPersonalizariAPI.Repositories.GenericRepository;
+
+namespace AtelierPersonalizariAPI.Repositories.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private AtelierPersonalizariContext _context;
+        private GenericRepository<ProductTypeProvider> productTypeProviderRepository;
+        private GenericRepository<ProductType> productTypeRepository;
+        private GenericRepository<Product> productRepository;
+
+        public UnitOfWork(AtelierPersonalizariContext context)
+        {
+            _context = context;
+        }
+
+
+        public GenericRepository<ProductTypeProvider> ProductTypeProviderRepository
+        {
+            get
+            {
+                if (this.productTypeProviderRepository == null)
+                {
+                    this.productTypeProviderRepository = new GenericRepository<ProductTypeProvider>(_context);
+                }
+                return productTypeProviderRepository;
+            }
+        }
+
+        public GenericRepository<ProductType> ProductTypeRepository
+        {
+            get
+            {
+                if (this.productTypeRepository == null)
+                {
+                    this.productTypeRepository = new GenericRepository<ProductType>(_context);
+                }
+                return productTypeRepository;
+            }
+        }
+
+        public GenericRepository<Product> ProductRepository
+        {
+            get
+            {
+                if (this.productRepository == null)
+                {
+                    this.productRepository = new GenericRepository<Product>(_context);
+                }
+                return productRepository;
+            }
+        }
+
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
